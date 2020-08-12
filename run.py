558a1b428779eb3e8eeee3cc16de57f75d177fec
Mucral <43535702+Mucral@users.noreply.github.com>
2020-08-12 20:27:28 +0200

    rebuild

--- run.py
+++ run.py
@@ -3,21 +3,14 @@
 import time
 import main
 import database
-# import requests
-# from bs4 import BeautifulSoup
+import requests
 
 
 def home():
     sql = """SELECT * FROM site GROUP BY web"""
     results = database.select(sql)
     for result in results:
-        parse(result[3])
+        # parse(result[3])
+        print(result)
     time.sleep(2)
     main.main()
-
-
-def parse(url):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    name = soup.find_all(class_="result")
-    print(name)
