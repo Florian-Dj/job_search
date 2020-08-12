cfb290607f4ab66675236172b658ae5567433cc8
Mucral <43535702+Mucral@users.noreply.github.com>
2020-08-12 18:17:31 +0200

    Check list search

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 import time
 import database
+import site
 
 
-def main():
+def home():
     print("""
     1 - Run
-    2 - Ajouter Lien
-    3 - Supprimer Lien
-    4 - Liste Lien
-    0 - Quitter""")
+    2 - Site
+    3 - Annonces
+    0 - Quitter
+    """)
     choose = input("Votre action : ")
     try:
         choose = int(choose)
@@ -19,57 +20,15 @@ def main():
         elif choose == 1:
             run()
         elif choose == 2:
-            add_link()
+            site.home()
         elif choose == 3:
-            delete_link()
-        elif choose == 4:
-            list_link()
+            annonce()
     except ValueError:
         print("Merci de rentrer une donné valide !")
         time.sleep(2)
-        main()
-
-
-def add_link():
-    link = input("URL de la recherche : ")
-    print("""
-    1 - Pole-Emploi
-    2 - Linkedin
-    3 - Leboncoin
-    0 - Retour""")
-    web = input("De quel site provient la recherche ? ")
-    if web == "1":
-        web = "Pole-Emploi"
-    elif web == "2":
-        web = "Linkedin"
-    elif web == "3":
-        web = "Leboncoin"
-    else:
-        print("Ici")
-        main()
-    subject = input("Quel est le titre de la recherche ? ")
-    conn = database.connection()
-    sql = """INSERT INTO site (web, subject, link) VALUES ('{}', '{}', '{}')""".format(web, subject, link)
-    try:
-        data = conn.cursor()
-        data.execute(sql)
-        conn.commit()
-        conn.close()
-    except conn.Error as e:
-        print(e)
-    time.sleep(3)
-    main()
-
-
-def list_link():
-    sql = """SELECT * FROM site"""
-    results = database.select(sql)
-    for result in results:
-        print(result)
-    time.sleep(2)
-    main()
+        home()
 
 
 if __name__ == '__main__':
     database.create_db()
-    main()
+    home()
---
+++ site.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import time
+import main
+import database
+
+
+def home():
+    print("""
+    1 - Ajouter Lien
+    2 - Supprimer Lien
+    3 - Liste Lien
+    0 - Retour
+    """)
+    choose = input("Votre action : ")
+    try:
+        choose = int(choose)
+        if choose == 0:
+            main.home()
+        elif choose == 1:
+            add_link()
+        elif choose == 2:
+            delete_link()
+        elif choose == 3:
+            list_link()
+    except ValueError:
+        print("Merci de rentrer une donné valide !")
+        time.sleep(2)
+        home()
+
+
+def add_link():
+    link = input("URL de la recherche : ")
+    print("""
+    1 - Pole-Emploi
+    2 - Linkedin
+    3 - Leboncoin
+    0 - Retour""")
+    web = input("De quel site provient la recherche ? ")
+    if web == "1":
+        web = "Pole-Emploi"
+    elif web == "2":
+        web = "Linkedin"
+    elif web == "3":
+        web = "Leboncoin"
+    else:
+        print("Ici")
+        home()
+    subject = input("Quel est le titre de la recherche ? ")
+    conn = database.connection()
+    sql = """INSERT INTO site (web, subject, link) VALUES ('{}', '{}', '{}')""".format(web, subject, link)
+    try:
+        data = conn.cursor()
+        data.execute(sql)
+        conn.commit()
+        conn.close()
+    except conn.Error as e:
+        print(e)
+    time.sleep(3)
+    home()
+
+
+def delete_link():
+    sql = """SELECT * FROM site"""
+    results = database.select(sql)
+    for result in results:
+        print(result)
+    print("0 - Retour")
+    choose = input("Quel cherche voulez-vous supprimer ? ")
+    if choose == "0":
+        home()
+
+def list_link():
+    sql = """SELECT * FROM site"""
+    results = database.select(sql)
+    for result in results:
+        print()
+        print("Site : {}".format(result[1]))
+        print("Poste : {}".format(result[2]))
+        print("Lien : {}".format(result[3]))
+    time.sleep(2)
+    home()
