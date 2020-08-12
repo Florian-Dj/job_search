019704d055f9a1990d16819831b891978c273562
Mucral <43535702+Mucral@users.noreply.github.com>
2020-08-12 14:05:27 +0200

    Create main page python

---
+++ main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+
+def main():
+    print("""
+    1 - Run
+    2 - Ajouter Lien
+    3 - Supprimer Lien
+    4 - Liste Lien
+    0 - Quitter""")
+    choose = input("Votre action : ")
+    try:
+        choose = int(choose)
+        if choose == 0:
+            exit()
+        elif choose == 1:
+            add_link()
+        elif choose == 2:
+            delete_link()
+        elif choose == 3:
+            list_link()
+    except ValueError:
+        print("Merci de rentrer une donné valide !")
+        time.sleep(2)
+        main()
+
+
+def list_link():
+
+
+if __name__ == '__main__':
+    main()
