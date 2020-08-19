a43ffff605dac24c5e853c9fc96fe295803ca974
Mucral <43535702+Mucral@users.noreply.github.com>
2020-08-19 17:11:18 +0200

    Set status if ad is 'non_lu'

--- ad.py
+++ ad.py
@@ -85,35 +85,53 @@ def list_search(web, status, nb):
         if choose == 0:
             choose_site(status)
         elif 2 <= choose <= len(results):
-            list_ad(web, results[choose-1], status)
+            list_ad(web, results[choose-1], status, nb)
         else:
             print("Merci de rentrer une donné valide !")
             time.sleep(2)
-            list_search(web, status)
+            list_search(web, status, nb)
     except ValueError:
         print("Merci de rentrer une donné valide !")
         time.sleep(2)
-        list_search(web, status)
+        list_search(web, status, nb)
 
 
-def list_ad(site, subject, status):
+def list_ad(site, subject, status, nb):
     config.read("config.ini")
-    sql = """SELECT ad.title, ad.description, ad.location, ad.link, search.web, status FROM ad
+    sql = """SELECT ad.id, ad.title, ad.description, ad.location, ad.link, search.web, status FROM ad
             LEFT JOIN search ON ad.site_id = search.id
             WHERE site_id={} AND status='{}'""".format(subject[0], status)
     results = database.select(sql)
     if results:
+        conn = database.connection()
         print("\n----- ({}) Annonces pour {} / {} -----\n".format(len(results), site, subject[2]))
         for result in results:
-            if result[5] != "non_lu":
+            if result[6] != "non_lu":
                 time.sleep(int(config["DEFAULT"]["cooldown_ad"]))
-                print("\nLien : {}\nTitre : {}\nLieu : {}\nDescription : {}\n".format(result[3], result[0], result[2], result[1]))
+                print("\nLien : {}\nTitre : {}\nLieu : {}\nDescription : {}\n".format(result[4], result[1], result[3], result[2]))
             else:
-                print("\nLien : {}\nTitre : {}\nLieu : {}\nDescription : {}\n".format(result[3], result[0], result[2], result[1]))
-                print("Choix du status")
-                time.sleep(2)
+                change_status(result, conn)
+        conn.close()
     else:
         print("\n----- (0) Annonces pour {} / {} -----\n".format(site, subject[2]))
     time.sleep(2)
-    list_search(site, status)
+    list_search(site, status, nb)
 
+
+def change_status(result, conn):
+    print("\nLien : {}\nTitre : {}".format(result[4], result[1], result[3], result[2]))
+    print("1 - Postulée \t 2 - Inadequate \t 3 - Expirée \t 4 - Rien")
+    choose = input("Votre choix : ")
+    print()
+    choose = int(choose)
+    text = ["postulée", "inadequate", "expirée"]
+    if 1 <= choose <= 3:
+        sql = """UPDATE ad SET status='{}' WHERE id={}""".format(text[choose-1], result[0])
+        conn.execute(sql)
+        conn.commit()
+    elif choose == 4:
+        pass
+    else:
+        print("Merci de rentrer une donné valide !")
+        time.sleep(2)
+        change_status(result, conn)
