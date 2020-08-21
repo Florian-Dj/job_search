734eb74764a7d5c3636ba59f161a7ebef13cb5a3
Mucral <43535702+Mucral@users.noreply.github.com>
2020-08-21 15:32:35 +0200

    Add directory templates (home, ad and search page html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return render(request, 'home.html')
 # Create your views here.
+
+
+def search(request):
+    return HttpResponse("Toutes les recherches ici même !")
+
+
+def ad(request):
+    return  HttpResponse("Toutes les annonces sont ici")
