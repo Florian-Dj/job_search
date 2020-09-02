b2e4c602617dba1898900d9fa17f3823a1427872
Mucral <43535702+Mucral@users.noreply.github.com>
2020-09-02 13:29:32 +0200

    Delete Folder mysite and rename folder mysite/mysite into admin

--- mysite/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('recherche', views.search, name='recherche'),
-    path('annonce', views.ad, name='annonce')
-]
