d8fbfec055b22c91d3dd994c011d6e479d2b0e08
Mucral <43535702+Mucral@users.noreply.github.com>
2020-11-30 19:12:09 +0100

    Bug fix urls and add collectstatic on run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ then
         touch data.db
         python3 manage.py migrate
         python3 manage.py createsuperuser
+        python3 manage.py collectstatic
 fi
 
 source env/bin/activate && screen -dmS django-jobs python manage.py runserver  --insecure 0:8000
